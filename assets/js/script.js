521826502934599e228ad2d4a681e21171bdc770
thyago_lbt <thyago14@outlook.fr>
2019-10-23 16:37:53 +0200

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function verification(age){
+  var age = document.getElementById('age').value;
+  if (age>=18) {
+    alert ('vous êtes majeur');
+  } else {
+    alert ('vous êtes mineur');
+  }
+}
